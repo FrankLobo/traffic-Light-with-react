a820b6ba8b3c0f3f02f4d230e66e3f2a74b2847f
FrankLobo <frankeloboq@gmail.com>
2022-02-03 19:21:25 +0000

    Traffic ligth finish

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,24 +5,28 @@ import React, { useState } from "react";
 //create your first component
 const TrafficLight = () => {
 	const [color, setColor] = useState("");
+
+	// Usage
+	//   <button onClick={sayHello}>Default</button>;
 	return (
 		<>
 			<div className="base-light"></div>
 			<div className="traffic-light">
 				<div
-					className={"light red" + (color === "red" ? " glow" : "")}
+					className={"light red" + (color === "red" ? " glow1" : "")}
 					onClick={() => setColor("red")}></div>
 				<div
 					className={
-						"light yellow" + (color === "yellow" ? " glow" : "")
+						"light yellow" + (color === "yellow" ? " glow2" : "")
 					}
 					onClick={() => setColor("yellow")}></div>
 				<div
 					className={
-						"light green" + (color === "green" ? " glow" : "")
+						"light green" + (color === "green" ? " glow3" : "")
 					}
 					onClick={() => setColor("green")}></div>
 			</div>
+			;
 		</>
 	);
 };
