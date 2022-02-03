935c73f4763209e41cbe73a190ad7de190fd2d56
FrankLobo <frankeloboq@gmail.com>
2022-02-03 16:12:54 +0000

    cambio en luces

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,30 @@
-import React from "react";
-
+import React, { useState } from "react";
 //include images into your bundle
 // import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const TrafficLight = () => {
+	const [color, setColor] = useState("");
 	return (
-		<div className="traffic-light">
-			<div className="light red"></div>
-			<div className="light yellow"></div>
-			<div className="light green"></div>
-		</div>
-	)
+		<>
+			<div className="base-light"></div>
+			<div className="traffic-light">
+				<div
+					className={"light red" + (color === "red" ? " glow" : "")}
+					onClick={() => setColor("red")}></div>
+				<div
+					className={
+						"light yellow" + (color === "yellow" ? " glow" : "")
+					}
+					onClick={() => setColor("yellow")}></div>
+				<div
+					className={
+						"light green" + (color === "green" ? " glow" : "")
+					}
+					onClick={() => setColor("green")}></div>
+			</div>
+		</>
+	);
 };
 
 export default TrafficLight;
